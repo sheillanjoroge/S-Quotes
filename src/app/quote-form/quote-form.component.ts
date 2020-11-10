a4fb11551391c7c26b51ed645bf1121395ecf393
sheillanjoroge <sheillan.njoroge@gmail.com>
2020-11-10 15:37:48 +0300

    add form layout

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote} from '../quote';
+
 
 @Component({
   selector: 'app-quote-form',
@@ -6,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+
+
+  newQuote = new Quote ("", "", "",new Date())
+
+@Output()addQuote = new EventEmitter<Quote>();
+
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+  }
+
 
   constructor() { }
 
