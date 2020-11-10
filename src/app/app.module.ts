e9e05bef951bbff7d18f1a593553b85317e297c8
sheillanjoroge <sheillan.njoroge@gmail.com>
2020-11-10 15:18:49 +0300

    add form structure and design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormModule} from '@angular/forms';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
+    QuoteFormComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,19 @@
 export class Quote {
+    name: string;
+    quote: string;
+    author: string;
+    date: Date;
+    upVote: number;
+    downVote:number;
+  
+    constructor(name: string, quote:string, author:string, date:Date){
+        
+    this.name=name;
+    this.quote=quote;
+    this.author=author;
+    this.date=date;
+    this.upVote=0;
+    this.downVote=0;
+    }
+  
 }
